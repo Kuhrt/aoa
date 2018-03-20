ed1ae66718f6a227e061c2d6bbf78425c959c09e
Kuhrt Cowan <kuhrt.cowan@gmail.com>
2018-03-19 22:29:45 -0500

    Finished desktop styling

--- web/app/themes/anglin-outdoor/resources/assets/scripts/routes/home.js
+++ web/app/themes/anglin-outdoor/resources/assets/scripts/routes/home.js
@@ -9,14 +9,18 @@ export default {
     const map = $('.home-billboards__map');
     // If section is swiped on
     hammertime.on('swipeleft', function() {
-      map.animate({
-        left: '0%',
-      }, 300);
+      if ($(window).width() < 1200) {
+        map.animate({
+          left: '0%',
+        }, 300);
+      }
     });
     hammertime.on('swiperight', function() {
-      map.animate({
-        left: '100%',
-      }, 300);
+      if ($(window).width() < 960) {
+        map.animate({
+          left: '100%',
+        }, 300);
+      }
     });
 
     $('.home-billboards-map__header a').on('click', e => {
@@ -26,6 +30,52 @@ export default {
         left: '100%',
       }, 300);
     });
+
+
+    // NAVIGATION
+    // Brand Logo
+    $(".banner a.brand").on('click', e => {
+      e.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+
+    // Smooth Scrolling thanks to CSS-Tricks
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top,
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            });
+          }
+        }
+      });
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
