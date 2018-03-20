72ea41eb708412f068266c6a7fc867f40f2f9a2a
Kuhrt Cowan <kuhrt.cowan@gmail.com>
2018-03-19 23:48:53 -0500

    Added smooth scrolling. Added tilt effect on header. Added navigation functionality

--- web/app/themes/anglin-outdoor/resources/assets/scripts/routes/home.js
+++ web/app/themes/anglin-outdoor/resources/assets/scripts/routes/home.js
@@ -1,4 +1,7 @@
+/* eslint-disable */
 import Hammer from 'hammerjs';
+import 'waypoints/lib/jquery.waypoints.min.js';
+import 'tilt.js';
 
 export default {
   init() {
@@ -37,6 +40,58 @@ export default {
     $(".banner a.brand").on('click', e => {
       e.preventDefault();
       $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+
+    // Adding nav item class
+    new Waypoint({
+      element: $('#about'),
+      handler: direction => {
+        if (direction === 'down') {
+          // Removing active class
+          removeActiveClass();
+          // Adding active class
+          $('nav ul li a[href=\'#about\']').addClass('active');
+        } else if (direction === 'up') {
+          // Removing active class
+          removeActiveClass();
+        }
+      },
+    });
+    new Waypoint({
+      element: $('#billboards'),
+      handler: direction => {
+        if (direction === 'down') {
+          // Removing active class
+          removeActiveClass();
+          // Adding active class
+          $('nav ul li a[href=\'#billboards\']').addClass('active');
+          $('.brand img').addClass('billboard');
+        } else if (direction === 'up') {
+          // Removing active class
+          removeActiveClass();
+          $('.brand img').removeClass('billboard');
+          // Adding active class
+          $('nav ul li a[href=\'#about\']').addClass('active');
+        }
+      },
+    });
+    new Waypoint({
+      element: $('#contact'),
+      handler: direction => {
+        if (direction === 'down') {
+          // Removing active class
+          removeActiveClass();
+          $('.brand img').removeClass('billboard');
+          // Adding active class
+          $('nav ul li a[href=\'#contact\']').addClass('active');
+        } else if (direction === 'up') {
+          // Removing active class
+          removeActiveClass();
+          $('.brand img').addClass('billboard');
+          // Adding active class
+          $('nav ul li a[href=\'#billboards\']').addClass('active');
+        }
+      },
     });
 
     // Smooth Scrolling thanks to CSS-Tricks
@@ -79,5 +134,16 @@ export default {
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
+    $('.home__top').tilt({
+      maxTilt    : 5,
+      perspective: 1000,
+    });
   },
 };
+
+
+function removeActiveClass() {
+  $('nav ul li a[href=\'#about\']').removeClass('active');
+  $('nav ul li a[href=\'#billboards\']').removeClass('active');
+  $('nav ul li a[href=\'#contact\']').removeClass('active');
+}
